a921ee0d2cb3181be4c8e10d3983620725544b03
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-26 20:04:07 +0900

    [update]ダウンロード機能

--- js/events.js
+++ js/events.js
@@ -1,6 +1,19 @@
 "use strict";
 
-const deleteAllCanvas = () => {
+
+function clearAllCanvas() {
     tmp_ctx.clearRect(0, 0, tmp_canvas.width, tmp_canvas.height);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function downloadCanvas() {
+    // ダウンロードリンク作成
+    const link = document.createElement("a");
+    link.href = canvas.toDataURL("iamge/png");
+
+    // yyyy_mm_dd.pngの形でダウンロードさせる
+    const date = new Date();
+    const fileName = `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}`;
+    link.download = fileName + ".png";
+    link.click();
 }
